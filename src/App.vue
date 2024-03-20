0344664eee78133407d1686f2c98190c41cd5e7d
fraberret <f.berretta02@gmail.com>
2024-03-20 15:31:16 +0100

    add 'hello word' in page

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <script>
+export default {
+  data() {
+    return {
+      message: 'hello world'
+    }
+
+  }
+
+}
 </script>
 
 <template>
-
+  <h1>{{ message }}</h1>
 </template>
 
 <style></style>
